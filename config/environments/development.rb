60394daa7049735b0058900da5a353f062303c05
関祐樹 <greenpeace623@docomo.ne.jp>
2020-08-07 22:05:39 +0900

    password reset add

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,9 +28,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :test
-  # host = 'localhost:3000'                     # ローカル環境
-  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   config.action_mailer.delivery_method = :smtp
   host = '<your heroku app>.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
